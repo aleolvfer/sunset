d545e736e0854ff7c09a1bd838dade1cc52b2502
aleolvfer <aleolvfer@hotmail.com>
2020-10-02 03:06:31 -0300

    capturando dados da da api sunrise-sunset

--- client/index.js
+++ client/index.js
@@ -1,8 +1,3 @@
-function _handleErrors(res){
-    if(!res.ok) throw new Error(res.statusText + "ErrorOpenStreetMapAPI");
-    return res;
-}
-
 const fetchEndereco = async endereco => {
     const response = await fetch(`https://nominatim.openstreetmap.org/search/?q=${endereco}&format=json`);
     return response.json(); 
@@ -10,23 +5,20 @@ const fetchEndereco = async endereco => {
 
 const objEndereco = async () => {
     const endereco = document.querySelector('#endereco').value;
-    const objetoComDadosDoEndereco = await fetchEndereco(endereco);
-    console.log(objetoComDadosDoEndereco);
+    const response = await fetchEndereco(endereco);
+    const objetoComDadosDoEndereco = response[0];
+    const coordenadas = [parseFloat(objetoComDadosDoEndereco.lat), parseFloat(objetoComDadosDoEndereco.lon)];
+    return coordenadas;
 }
 
-document.querySelector('#search').addEventListener("click", function(event){
+const fetchSunriseSunset = async (coordenadas, date) => {
+    const url = `http://api.sunrise-sunset.org/json?lat=${coordenadas[0]}&lng=${coordenadas[1]}&date=${date}`
+    const response = await fetch(url);
+    console.log(await response.json());
+}
+
+document.querySelector('#search').addEventListener("click", async function(event){
     event.preventDefault();
-    objEndereco();
+    fetchSunriseSunset(await objEndereco(), '2020-10-01');
 });
-
-
-
-
-
-// fetch("https://www.potterapi.com/v1/characters?key=$2a$10$e5F22Rwiu/Ysm62jg9Yx3er/xz7TppSb82QKTTS3lRmikLTddM/GC")
-//     .then( res => _handleErrors(res))
-//     .then( res => res.json())
-//     .then( json => {
-//         console.log(json[109]);
-//     });
 
