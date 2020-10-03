d986626b47cf554e067aec94ec6e6d151bc59952
aleolvfer <aleolvfer@hotmail.com>
2020-10-03 01:23:57 -0300

    capturing only necessary data

--- client/index.js
+++ client/index.js
@@ -1,24 +1,35 @@
-const fetchEndereco = async endereco => {
-    const response = await fetch(`https://nominatim.openstreetmap.org/search/?q=${endereco}&format=json`);
+const fetchAddress = async address => {
+    const response = await fetch(`https://nominatim.openstreetmap.org/search/?q=${address}&format=json`);
     return response.json(); 
 }
 
 const objEndereco = async () => {
     const endereco = document.querySelector('#endereco').value;
-    const response = await fetchEndereco(endereco);
-    const objetoComDadosDoEndereco = response[0];
-    const coordenadas = [parseFloat(objetoComDadosDoEndereco.lat), parseFloat(objetoComDadosDoEndereco.lon)];
-    return coordenadas;
+    const response = await fetchAddress(endereco);
+    const coordinates = [parseFloat(response[0].lat), parseFloat(response[0].lon)];
+    return coordinates;
 }
 
-const fetchSunriseSunset = async (coordenadas, date) => {
-    const url = `http://api.sunrise-sunset.org/json?lat=${coordenadas[0]}&lng=${coordenadas[1]}&date=${date}`
+const fetchSunriseSunsetTime = async (coordinates, date) => {
+    const url = `http://api.sunrise-sunset.org/json?lat=${coordinates[0]}&lng=${coordinates[1]}&date=${date}`
     const response = await fetch(url);
-    console.log(await response.json());
+    return response.json();
+}
+
+const sunriseSunsetTime = (responseSunriseSunset) => {
+    const sunriseSunsetTime = {
+        sunrise : responseSunriseSunset.results.sunrise,
+        sunset : responseSunriseSunset. results.sunset,
+        dayLength : responseSunriseSunset.results.day_lenght,
+        firstLight : responseSunriseSunset.results.civil_twilight_begin,
+        lastLight : responseSunriseSunset.results.civil_twilight_end 
+    }
+    // return sunriseSunsetTime;
+    console.log(sunriseSunsetTime)
 }
 
 document.querySelector('#search').addEventListener("click", async function(event){
     event.preventDefault();
-    fetchSunriseSunset(await objEndereco(), '2020-10-01');
+    sunriseSunsetTime(await fetchSunriseSunsetTime(await objEndereco(), '2020-10-03'))
 });
 
