6208e1340541c0209344760567ccae61b3917607
aleolvfer <aleolvfer@hotmail.com>
2020-10-08 02:33:34 -0300

    Aplicando conceitos de Debounce nas chamadas

--- js/index.js
+++ js/index.js
@@ -3,14 +3,16 @@ const fetchAddress = async address => {
     return response.json(); 
 }
 
-const objEndereco = async () => {
+const getCoordinates = async () => {
     const address = document.querySelector('#busca-endereco').value;
     const response = await fetchAddress(address);
     const coordinates = [parseFloat(response[0].lat), parseFloat(response[0].lon)];
     return coordinates;
 }
 
-const fetchSunriseSunsetTime = async (coordinates, date) => {
+const fetchSunriseSunsetTime = async (coordinates) => {
+    const objDate = new Date();
+    const date = `${objDate.getFullYear()}/${objDate.getMonth()+1}/${objDate.getDate()}`;
     const url = `https://api.sunrise-sunset.org/json?lat=${coordinates[0]}&lng=${coordinates[1]}&date=${date}`
     const response = await fetch(url);
     return response.json();
@@ -18,7 +20,7 @@ const fetchSunriseSunsetTime = async (coordinates, date) => {
 
 const formatedTime = time => {
     const timezone = { AC: 5, AL: 3, AP: 3, AM: 4, BA: 3, CE: 3, DF: 3, ES: 3, GO: 3, MA: 3, MT: 4, MS: 4, MG: 3,
-         PA: 4, PB: 3, PR: 3, PE: 3, PI: 3, RJ: 3, RN: 3, RS: 3, RO: 4, RR: 4, SC: 3, SP: 3, SE: 3, TO: 3 };
+         PA: 4, PB: 3, PR: 3, PE: 3, PI: 3, RJ: 3, RN: 3, RS: 3, RO: 4, RR: 4, SC: 3, SP: 3, SE: 3, TO: 3, "": 3 };
     const us = document.querySelector('#busca-estado').value;
     let x;
     for(x in timezone){
@@ -40,7 +42,6 @@ const sunriseSunsetTime = (responseSunriseSunset) => {
         lastLight : formatedTime(responseSunriseSunset.results.civil_twilight_end)
     }
     return sunriseSunsetTime;
-    // console.log(sunriseSunsetTime)
 }
 
 const liCreate = object => {
@@ -49,15 +50,38 @@ const liCreate = object => {
     return li;
 }
 
-document.querySelector('#search').addEventListener("click", async function(event){
+
+let time = null;
+let stateApp = '';
+const runApp = async function(event) {
     event.preventDefault();
-    const sunriseSunsetTimeObject = sunriseSunsetTime(await fetchSunriseSunsetTime(await objEndereco(), '2020-10-03'))
 
-    const arrayData = Object.entries(sunriseSunsetTimeObject);
-    for(var [key, value] of arrayData){
-        let li = key + " " + value;
-        document.querySelector('#dados').appendChild(liCreate(li));
-    }
+    clearTimeout(time);
+    time = setTimeout(async () => {
 
-});
+        const addressArray = await getCoordinates();
+        const log = addressArray.toString();
+        
+        if(log != stateApp){
+            stateApp = addressArray.toString();
 
+            const sunriseSunsetTimeObject = sunriseSunsetTime(await fetchSunriseSunsetTime(addressArray));
+
+            const arrayOfObj = Object.entries(sunriseSunsetTimeObject);
+        
+            const parametersArray = ['Nascer do Sol ', 'Por do Sol ', 
+            'Duração do Dia ', 'Primeira Luz do Dia ', 'Última Luz do dia '];
+        
+            let cont = 0;
+            for(var [key, value] of arrayOfObj){
+                let li = parametersArray[cont] + value;
+                document.querySelector('#dados').appendChild(liCreate(li));
+                cont++;
+            }
+        }
+        
+    }, 1000);
+}
+
+
+document.querySelector('#search').addEventListener("click", runApp);
