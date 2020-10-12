66120cea6d6f1614da95941c7c341bd63ca39cac
aleolvfer <aleolvfer@hotmail.com>
2020-10-12 01:16:56 -0300

    Ainda trabalhando na apresentacao

---
+++ js/view/parallax.js
@@ -0,0 +1,20 @@
+const translate = document.querySelectorAll('.translate');
+const bigTitle = document.querySelector('.big-title');
+const description = document.querySelector(".description")
+const btnScroll = document.querySelector("header a");
+const header = document.querySelector("body");
+
+let header_height = header.offsetHeight;
+
+window.addEventListener('scroll', () => {
+    let scroll = window.pageYOffset
+
+    translate.forEach(element => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateY(${scroll * speed}px)`;
+    })
+
+    bigTitle.style.opacity = - (scroll*2) / (header_height / 2) + 1;
+    description.style.opacity = - (scroll*2) / (header_height / 2) + 1;
+    btnScroll.style.opacity = - (scroll*2) / (header_height / 2) + 1;
+})
